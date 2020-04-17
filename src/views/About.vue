2d7f219f7d1a3196eeeaf2816ab192708c84fbc1
denyncrawford <kiquerangelb@gmail.com>
2020-04-17 11:11:35 -0300

    1.0.0 publish

--- src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="about">
-      <div class="heading-container">
-        <h1 class="main-heading is-animated">This is<br><span class="green-heading-bg">the about</span><br>screen.<br></h1>
-        <img src="../assets/hand-2.png" class="main-img is-animated">
-      </div>
-  </div>
-</template>
